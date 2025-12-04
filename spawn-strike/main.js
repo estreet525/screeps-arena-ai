972048461e933ec56ec1352ff96d92599acb3c4f
Eric <e.street525@gmail.com>
2025-12-03 20:14:42 -0500

    Baseline Spawn Strike code.

---
+++ spawn-strike/main.js
@@ -0,0 +1,16 @@
+import { getObjectsByPrototype } from 'game/utils';
+import { StructureSpawn } from 'game/prototypes';
+import { ATTACK, MOVE, ERR_NOT_IN_RANGE } from 'game/constants';
+
+const mySpawn = getObjectsByPrototype(StructureSpawn).find(spawn => spawn.my);
+const enemySpawn = getObjectsByPrototype(StructureSpawn).find(spawn => !spawn.my);
+var creep;
+
+export function loop() {
+    if(!creep) {
+        creep = mySpawn.spawnCreep([ATTACK, ATTACK, MOVE, MOVE]).object;
+    }
+    if(creep.attack(enemySpawn) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(enemySpawn);
+    }
+}
